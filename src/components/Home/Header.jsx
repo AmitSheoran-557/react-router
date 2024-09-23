e91b83af8affa63b72bbec7756753f85ca114d7a
AmitSheoran-557 <amit22082006@gmail.com>
2024-09-23 10:44:32 +0530

    first commit

---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <section>
+                <div className='container'>
+                    <nav className='d-flex mt-4 p-3 justify-content-between align-items-center '>
+                        <div>logo</div>
+                        <div className='d-flex gap-3 text-decoration-none list-unstyled'>
+                            <a className="text-decoration-none text-black" href="/"><li>app</li></a>
+                            <a className="text-decoration-none text-black" href="/"><li>link</li></a>
+                            <a className="text-decoration-none text-black" href="/"><li>link</li></a>
+                            <a className="text-decoration-none text-black" href="/"><li>link</li></a>
+                            <a className="text-decoration-none text-black" href="/"><li>link</li></a>
+                            <a className="text-decoration-none text-black" href="/"><li>link</li></a>
+                        </div>
+                    </nav>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Header
---
+++ src/views/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Second/Header'
+
+const About = () => {
+  return (
+    <div><Header/></div>
+  )
+}
+
+export default About
